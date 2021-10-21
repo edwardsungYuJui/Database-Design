9444e1b74f66eee6ab2b511a2cfd6b7648989d8b
Edward Sung <edwardsung63@gmail.com>
2021-10-21 16:41:01 -0700

    ex9_03

--- Desktop/Fall 2021/IS455/Sung_Edward_mgs89/ex9_03.sql
+++ Desktop/Fall 2021/IS455/Sung_Edward_mgs89/ex9_03.sql
@@ -1,6 +1,10 @@
 USE my_guitar_shop;
 SELECT 
-    card_number, LENGTH(card_number), RIGHT(card_number, 4) AS "Last Four Digits",
-    
+    card_number,
+    LENGTH(card_number),
+    RIGHT(card_number, 4) AS 'Last Four Digits',
+    IF(LENGTH(card_number) = 16,
+        CONCAT('XXXX-XXXX-XXXX-', RIGHT(card_number, 4)),
+        CONCAT('XXXX-XXXXXX-X', RIGHT(card_number, 4))) AS "Digits Displayed"
 FROM
     orders;
---
+++ Desktop/Fall 2021/IS455/Sung_Edward_mgs89/ex9_04.sql
@@ -0,0 +1,12 @@
+USE my_guitar_shop;
+SELECT 
+    order_id,
+    order_date,
+    DATE_ADD(order_date, INTERVAL 2 DAY) AS approx_ship_date,
+    DATEDIFF(ship_date, order_date) AS days_to_ship
+FROM
+    orders
+WHERE
+    EXTRACT(MONTH FROM order_date) = 3 AND
+    ship_date IS NOT NULL;
+    
