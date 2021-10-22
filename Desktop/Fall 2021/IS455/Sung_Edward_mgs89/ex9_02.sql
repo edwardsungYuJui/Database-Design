29bb7d7f8286d1e5da0fd643ce036f897012f0b6
Edward Sung <edwardsung63@gmail.com>
2021-10-22 13:53:25 -0700

    final

--- Desktop/Fall 2021/IS455/Sung_Edward_mgs89/ex9_02.sql
+++ Desktop/Fall 2021/IS455/Sung_Edward_mgs89/ex9_02.sql
@@ -3,7 +3,7 @@ SELECT
     order_date,
     DATE_FORMAT(order_date, '%Y') AS 'Year',
     DATE_FORMAT(order_date, '%b-%d-%Y') AS 'Full Date',
-    DATE_FORMAT(order_date, '%l:%i %p') AS 'Date Time 12',
+    DATE_FORMAT(order_date, '%H:%i %p') AS 'Date Time 12',
     DATE_FORMAT(order_date, '%m/%d/%y %H:%i') AS 'Date Time 24'
 FROM
     orders;
